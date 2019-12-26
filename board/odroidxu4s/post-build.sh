1d5306b3affd1b225adc628655e3fa847a39e357
Etienne Garnier <garnier.etienne@gmail.com>
2019-12-26 15:32:21 +0100

    Breach the cloudbox CLI to many parts and refactor the setup process
    - app:domain to manage the public domain name
    - app:status to manage the Nextcloud instance
    - data:volumes to manage the hard disk volumes
    - system:gateway to manage the box gateway and escape NAT
    - system:hostname to manage the box name on the LAN
    - system:password to manage SSH passwords
    - system:setup to setup the box step by step

--- board/odroidxu4s/post-build.sh
+++ board/odroidxu4s/post-build.sh
@@ -8,8 +8,16 @@ rm -rf ${TARGET_DIR}/var/logs
 rm -rf ${TARGET_DIR}/usr/htdocs
 rm -rf ${TARGET_DIR}/etc/php-fpm.d
 
+# Add data volumes to fstab
+grep -qx '/dev/sda' foo.bar ||
+    echo "/dev/sda\t/mnt\t\tbtrfs\tdefaults\t0\t0" >> ${TARGET_DIR}/etc/fstab
+
 # Link nextcloud binary to /usr/bin
 ln -sf /var/www/app/occ ${TARGET_DIR}/usr/bin/occ
 
 # Link cloudbox-cli binary to /usr/bin
 ln -sf /opt/cloudbox-cli/bin/cloudbox ${TARGET_DIR}/usr/bin/cloudbox
+
+# Enable the setup app vhost
+ln -sf /etc/apache2/vhosts/available/setup.conf \
+    ${TARGET_DIR}/etc/apache2/vhosts/enabled/main.conf
